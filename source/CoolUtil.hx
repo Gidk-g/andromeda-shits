285590a6aac0c5e6d217314e272038ae972e3238
Gidk-g <gidkg302@gmail.com>
2023-03-05 16:39:08 +0100

    Update CoolUtil.hx

--- source/CoolUtil.hx
+++ source/CoolUtil.hx
@@ -68,32 +68,6 @@ class CoolUtil
 		return NORMAL;
 	}
 
-	public static function dominantColor(sprite:flixel.FlxSprite):Int{
-		var countByColor:Map<Int, Int> = [];
-		for(col in 0...sprite.frameWidth){
-			for(row in 0...sprite.frameHeight){
-			    var colorOfThisPixel:Int = sprite.pixels.getPixel32(col, row);
-			    if(colorOfThisPixel != 0){
-				    if(countByColor.exists(colorOfThisPixel)){
-				        countByColor[colorOfThisPixel] =  countByColor[colorOfThisPixel] + 1;
-				    }else if(countByColor[colorOfThisPixel] != 13520687 - (2*13520687)){
-					    countByColor[colorOfThisPixel] = 1;
-				    }
-			    }
-			}
-	    }
-		var maxCount = 0;
-		var maxKey:Int = 0;//after the loop this will store the max color
-		countByColor[flixel.util.FlxColor.BLACK] = 0;
-			for(key in countByColor.keys()){
-			if(countByColor[key] >= maxCount){
-				maxCount = countByColor[key];
-				maxKey = key;
-			}
-		}
-		return maxKey;
-	}
-
 	public static function addSprite(x,y,path:String,scrollFactor:Float=1):FlxSprite{
 		var sprite:FlxSprite = new FlxSprite(x, y).loadGraphic(Paths.image(path));
 		sprite.scrollFactor.set(scrollFactor, scrollFactor);
