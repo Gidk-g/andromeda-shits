b57a79f999c90226772c07c7ac0dbd274974acac
Gidk-g <gidkg302@gmail.com>
2023-03-21 17:17:36 +0100

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -97,7 +97,7 @@ class MainMenuState extends MusicBeatState
 		var versionShit:FlxText = new FlxText(5, FlxG.height - 18, 0, "Andromeda Engine " + Application.current.meta.get('version'), 12);
 		versionShit.scrollFactor.set();
 		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
-		add(versionShit);
+		// add(versionShit);
 
 		// NG.core.calls.event.logEvent('swag').send();
 
