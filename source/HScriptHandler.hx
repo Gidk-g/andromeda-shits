4cad1026c7781d90eeff9dd9cf96d13f4feea04a
Gidk-g <gidkg302@gmail.com>
2023-04-13 21:08:46 +0200

    Update HScriptHandler.hx

--- source/HScriptHandler.hx
+++ source/HScriptHandler.hx
@@ -120,7 +120,7 @@ class HScriptHandler
         });
 
         interp.variables.set("loadScript", function(script_path:String) {
-            var new_script = new HScriptHandler(TitleState.curDir + "/" + script_path + ".hx");
+            var new_script = new HScriptHandler(TitleState.curDir + "/" + script_path);
             new_script.start();
             
             if(createPost)
